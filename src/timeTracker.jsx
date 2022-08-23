252e520785e36f14620bf2ecf160f2e8a30a2c16
Curtis <brumfieldcurtis1996@gmail.com>
2022-08-23 14:48:37 -0500

    edited timeTracker

---
+++ src/timeTracker.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const TimeTracker = ()=>{
+    return(
+        <div className="timeTracker">
+            <div className="timeList">
+                 <div className="userInfo"></div>
+                 <div className="time"></div>
+            </div>
+            <div className="tracker">
+
+            </div>
+        </div>
+    );
+
+}
+
+export default TimeTracker;
