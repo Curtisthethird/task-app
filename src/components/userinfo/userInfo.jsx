0125d68ffedbddac9d6c12dc08953aeaa96b88a3
Curtis <brumfieldcurtis1996@gmail.com>
2022-08-23 15:28:04 -0500

    edited App and timeTracker

---
+++ src/components/userinfo/userInfo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import userImg from '../../images/avatar.png';
+
+
+const UserInfo = ()=>{
+    return(
+     <div className="userInfo">
+
+     </div>
+    );
+}
+
+export default UserInfo;
--- src/timeTracker.jsx
+++ src/timeTracker.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import UserInfo from "./components/userinfo/userInfo";
+import Time from "./components/time/time";
+
 
 const TimeTracker = ()=>{
     return(
         <div className="timeTracker">
             <div className="timeList">
-                 <div className="userInfo"></div>
+            <UserInfo />
+            <Time />
+               
                  <div className="time"></div>
             </div>
             <div className="tracker">
