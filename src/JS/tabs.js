3894dc732c4c35eb3522440f8c1d38c0de590631
Curtis <brumfieldcurtis1996@gmail.com>
2022-08-24 12:52:50 -0500

    styled tabs and app

--- src/JS/tabs.js
+++ src/JS/tabs.js
@@ -1,26 +1,21 @@
 const Tabs = ()=>{
 
-    const tabs= document.getElementsByClassName('tabs');
+    const tabs = document.getElementsByClassName('tab');
     const tabsContent = document.getElementsByClassName('tabContent');
 
-    for(i=0; i<tabs.length; i++){
+    for(let i=0; i<tabs.length; i++){
         if(tabs[i].classList.contains('active')){
-        let dataTab = tabs[i].getAttribute('data-tab');
-        for(j=0; j<tabsContent.length; j++){
-            tabsContent[j].classList.remove('active');
-            
-            if(tabsContent[j].id == dataTab){
-                tabsContent[j].classList.add('active');
+            let dataTab = tabs[i].getAttribute('data-tab');
+            for(let j=0; j<tabsContent.length; j++){
+                tabsContent[j].classList.remove('active');
 
+                if(tabsContent[j].id === dataTab){
+                    tabsContent[j].classList.add('active');
+                }
             }
-
         }
     }
-
-    }
-
-
-
+    
 }
 
 export default Tabs;
--- src/components/item/item.jsx
+++ src/components/item/item.jsx
@@ -4,15 +4,15 @@ import './item.css';
 
 const Item = (props)=>{
     return(
-        <div className="trackerItem">
+        <div className={"trackerItem "+props.title}>
             <div className='trackerInfo'>
                 <div className="header">
                 <span className="title">{props.title}</span>
                 <img src={iconEllipsis} alt="settings"/>
                 </div>
                 <div className="body">
-                    <span className="hours">{props.hours}</span>
-                    <span className="prevLog">{props.prevLog}</span>
+                    <span className="hours">{props.hours}hrs</span>
+                    <span className="prevLog">last week- {props.prevLog}hrs</span>
                 </div>
             </div>
         </div>
